8235f9aa57896a8545ee710c595a361a1af86d72
minhdev-ui <dominh162002@gmail.com>
2022-10-08 21:15:05 +0700

    update

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,15 +1,37 @@
-import { Stack, Typography } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
 import { Button, Input, Radio, Select } from "antd";
 import { Option } from "antd/lib/mentions";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Role } from "../constant";
-import { SyncOutlined } from "@ant-design/icons";
+import { CameraOutlined, SyncOutlined, UploadOutlined } from "@ant-design/icons";
+import { Box } from "@mui/system";
+
 
 const Profile = () => {
   const [username, setUsername] = useState(
     JSON.parse(localStorage.getItem("account")).username
   );
+  const [email, setEmail] = useState(
+    JSON.parse(localStorage.getItem("account")).email
+  );
+  const [fullName, setFullName] = useState(
+    JSON.parse(localStorage.getItem("account")).name
+  );
+  const [msv, setMsv] = useState(
+    JSON.parse(localStorage.getItem("account")).msv
+  );
+  const [password, setPassword] = useState(
+    JSON.parse(localStorage.getItem("account")).password
+  );
+  const [role, setRole] = useState(
+    JSON.parse(localStorage.getItem("account")).role
+  );
+  const [token] = useState(JSON.parse(localStorage.getItem("account")).token);
+  const [gender, setGender] = useState(
+    JSON.parse(localStorage.getItem("account")).gender
+  );
+
   return (
     <Stack sx={{ color: "#000", padding: "10px 20px" }}>
       <Stack
@@ -31,99 +53,43 @@ const Profile = () => {
             Account Setting
           </Typography>
         </Stack>
-        <Stack
-          direction="row"
-          flexWrap={"wrap"}
-          justifyContent="space-between"
-          sx={{ padding: "20px 0" }}
-          rowGap={4}
-        >
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Mã Sinh Viên
-            </Typography>
-            <Input value={username} />
+        <Stack>
+          <Stack width="20%" height={200} position='relative' sx={{
+            backgroundImage: 'url("/img/blank_image.jpg")',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            ':hover': {
+              '.overlay-img': {
+                opacity: 1
+              }
+            }
+          }}>
+            <Stack className="overlay-img" position='absolute' direction='column' justifyContent={'center'} alignItems='center' sx={{
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              inset: 0,
+              opacity: 0,
+              transition: 'all .3s',
+            }} component="label">
+              <CameraOutlined style={{fontSize: '28px', color: '#fff', cursor: 'pointer'}}/>
+              <Box
+                component='input'
+                type='file'
+                accept='image/png, image/jpeg'
+                hidden
+                width={100}
+                height={300}
+              />
+            </Stack>
           </Stack>
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Họ Tên
-            </Typography>
-            <Input value={username} />
-          </Stack>
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Username
-            </Typography>
-            <Input value={username} />
-          </Stack>
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Email
-            </Typography>
-            <Input value={username} />
-          </Stack>
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Password
-            </Typography>
-            <Input value={username} />
-          </Stack>
-          <Stack width="45%">
-            <Typography
-              component={"label"}
-              sx={{
-                fontSize: 16,
-                fontWeight: 500,
-                marginBottom: "10px",
-              }}
-            >
-              Chức Vụ
-            </Typography>
-            <Select>
-              {Role.map((item, i) => (
-                <Option key={i} value={item}>
-                  {item}
-                </Option>
-              ))}
-            </Select>
-          </Stack>
-          <Stack width="45%" direction={'row'} alignItems='center' gap={5}>
-            <Stack>
+          <Stack
+            direction="row"
+            flexWrap={"wrap"}
+            justifyContent="space-between"
+            sx={{ padding: "20px 0" }}
+            rowGap={4}
+          >
+            <Stack width="45%">
               <Typography
                 component={"label"}
                 sx={{
@@ -132,11 +98,37 @@ const Profile = () => {
                   marginBottom: "10px",
                 }}
               >
-                Lớp
+                Mã Sinh Viên
+              </Typography>
+              <Input value={msv} />
+            </Stack>
+            <Stack width="45%">
+              <Typography
+                component={"label"}
+                sx={{
+                  fontSize: 16,
+                  fontWeight: 500,
+                  marginBottom: "10px",
+                }}
+              >
+                Họ Tên
+              </Typography>
+              <Input value={fullName} />
+            </Stack>
+            <Stack width="45%">
+              <Typography
+                component={"label"}
+                sx={{
+                  fontSize: 16,
+                  fontWeight: 500,
+                  marginBottom: "10px",
+                }}
+              >
+                Username
               </Typography>
               <Input value={username} />
             </Stack>
-            <Stack>
+            <Stack width="45%">
               <Typography
                 component={"label"}
                 sx={{
@@ -145,18 +137,88 @@ const Profile = () => {
                   marginBottom: "10px",
                 }}
               >
-                Giới Tính
+                Email
               </Typography>
-              <Radio.Group>
-                <Radio value='Nam'>Nam</Radio>
-                <Radio value='Nữ'>Nữ</Radio>
-              </Radio.Group>
+              <Input value={email || ""} />
+            </Stack>
+            <Stack width="45%">
+              <Typography
+                component={"label"}
+                sx={{
+                  fontSize: 16,
+                  fontWeight: 500,
+                  marginBottom: "10px",
+                }}
+              >
+                Password
+              </Typography>
+              <Input value={password} type="password" />
+            </Stack>
+            <Stack width="45%">
+              <Typography
+                component={"label"}
+                sx={{
+                  fontSize: 16,
+                  fontWeight: 500,
+                  marginBottom: "10px",
+                }}
+              >
+                Chức Vụ
+              </Typography>
+              <Select defaultValue={role}>
+                {Role.map((item, i) => (
+                  <Option key={i} value={item}>
+                    {item}
+                  </Option>
+                ))}
+              </Select>
+            </Stack>
+            <Stack width="60%" direction={"row"} alignItems="center" gap={5}>
+              <Stack width="50%">
+                <Typography
+                  component={"label"}
+                  sx={{
+                    fontSize: 16,
+                    fontWeight: 500,
+                    marginBottom: "10px",
+                  }}
+                >
+                  Token
+                </Typography>
+                <Input value={token} width="fit-content" disabled />
+              </Stack>
+              <Stack>
+                <Typography
+                  component={"label"}
+                  sx={{
+                    fontSize: 16,
+                    fontWeight: 500,
+                    marginBottom: "10px",
+                  }}
+                >
+                  Giới Tính
+                </Typography>
+                <Radio.Group value={gender}>
+                  <Radio value={0}>Nam</Radio>
+                  <Radio value={1}>Nữ</Radio>
+                </Radio.Group>
+              </Stack>
             </Stack>
           </Stack>
         </Stack>
-        <Stack width="100%" direction={'row'} alignItems='center' justifyContent='flex-end' gap={1}>
-            <Button type="primary" danger>Cancel</Button>
-            <Button type="primary" icon={<SyncOutlined/>}>Update Account</Button>
+        <Stack
+          width="100%"
+          direction={"row"}
+          alignItems="center"
+          justifyContent="flex-end"
+          gap={1}
+        >
+          <Button type="primary" danger>
+            Cancel
+          </Button>
+          <Button type="primary" icon={<SyncOutlined />}>
+            Update Account
+          </Button>
         </Stack>
       </Stack>
     </Stack>
